ba979566cf41b01bbc0e27bd3289459b8a46d018
razonyang <razonyang@gmail.com>
2024-06-26 15:53:15 +0800

    feat: first commit

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+
+if [[ $3 != '' ]]
+then
+  cd $3
+fi
+
+# update modules
+if [[ $1 == 'true' ]]
+then
+  # update recursively
+  MODGET=$(hugo mod get -u ./...)
+else
+  MODGET=$(hugo mod get -u)
+fi
+
+echo "raw='$MODGET'" >> $GITHUB_OUTPUT
+
+# tidy modules
+if [[ $2 == 'true' ]]
+then
+  hugo mod tidy
+fi
